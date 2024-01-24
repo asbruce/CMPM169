d90641256a7502126a5443db58b5023791f1a42a
asbruce <keabruce@ucsc.edu>
2024-01-23 21:35:22 -0800

    Experiment 2
    Uploading my p5.js file for Experiment 2

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -1,67 +1,121 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Experiment 2 Vector Art, Animation, and Interactivity - Agate at Night
+// Author: Adrian Bruce
+// Date: 1/23/2024
 
-// Here is how you might set up an OOP p5.js project
-// Note that p5.js looks for a file called sketch.js
+//sources/reference used: http://www.generative-gestaltung.de/2/sketches/?01_P/P_2_2_3_01
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+//other thing i looked at for inspiration (but didn't include code from):http://www.generative-gestaltung.de/2/sketches/?01_P/P_2_0_03
 
-// Globals
-let myInstance;
-let canvasContainer;
+/**
+ * MOUSE
+ * click               : start a new agate slice
+ *
+ * KEYS
+ * f                   : freeze. loop on/off
+ * Delete/Backspace    : clear display
+ * s                   : save png
+ */
+'use strict';
+var colorPal = 1; //mine
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+var formResolution = 5; //i changed this from ref material
+var stepSize = 4; //i changed this from ref material
+var distortionFactor = 3; //i changed this from ref material
+var initRadius = 150;
+var centerX;
+var centerY;
+var x = [];
+var y = [];
 
-    myMethod() {
-        // code to run when method is called
-    }
-}
+var filled = false;
+var freeze = false;
 
-// setup() function is called once when the program starts
 function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
+  createCanvas(windowWidth, windowHeight);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+  // init shape - from ref material
+  centerX = width / 2;
+  centerY = height / 2;
+  var angle = radians(360 / formResolution);
+  for (var i = 0; i < formResolution; i++) {
+    x.push(cos(angle * i) * initRadius);
+    y.push(sin(angle * i) * initRadius);
+  }
+
+  background(0); //mine
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+  // calculate new points; based on ref material with modifications from me to make the shape change size
+  for (var i = 0; i < formResolution; i++) {
+    if(x[i]>0){
+      x[i] -= random(-stepSize/2, stepSize);
+    }else{
+      x[i] += random(-stepSize/2, stepSize);
+    }
+    if(y[i]>0){
+      y[i] -= random(-stepSize/2, stepSize);
+    }else{
+       y[i] += random(-stepSize/2, stepSize);
+    }
+  }
+
     noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
+    colorGen();
+  
+  //beginShape()-endShape() section is from ref material
+  beginShape(); 
+  // first controlpoint
+  curveVertex(x[formResolution - 1] + centerX, y[formResolution - 1] + centerY);
 
-// mousePressed() function is called once after every time a mouse button is pressed
+  // only these points are drawn
+  for (var i = 0; i < formResolution; i++) {
+    curveVertex(x[i] + centerX, y[i] + centerY);
+  }
+  curveVertex(x[0] + centerX, y[0] + centerY);
+
+  // end controlpoint
+  curveVertex(x[1] + centerX, y[1] + centerY);
+  endShape();
+}
+//next function from ref material, except where otherwise specified
 function mousePressed() {
-    // code to run when mouse is pressed
+  // create agate on mouse position
+  centerX = mouseX;
+  centerY = mouseY;
+  colorPal = random([1, 2, 3]) //mine
+  var angle = radians(360 / formResolution);
+  var radius = initRadius * random(0.5, 1);
+  for (var i = 0; i < formResolution; i++) {
+    x[i] = cos(angle * i) * initRadius;
+    y[i] = sin(angle * i) * initRadius;
+  }
+}
+//next function is mine. Selects a color palette for the agate slice
+function colorGen(){
+  switch(colorPal){
+    case 1:
+      fill(color(random(153,255), random(70,100), random(0, 40), random(0, 255)));
+      break;
+    case 2:
+      fill(color(random(0,50), random(90,190), random(0, 102), random(0, 255)));
+      break;
+    case 3:
+      fill(color(random(0, 40), random(20,200), random(190,255), random(0, 255)));
+      break;
+  }
+}
+//next function is from the reference material
+function keyReleased() {
+  if (key == 's' || key == 'S') saveCanvas(gd.timestamp(), 'png');
+  if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
+
+  // pause/play draw loop
+  if (key == 'f' || key == 'F') freeze = !freeze;
+  if (freeze) {
+    noLoop();
+  } else {
+    loop();
+  }
 }

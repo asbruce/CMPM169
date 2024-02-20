874515de659ca80fb39b5053c1b1703fbaeccc2d
asbruce <keabruce@ucsc.edu>
2024-02-19 21:56:48 -0800

    experiment 6

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -7,61 +7,133 @@
 
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+/**
+ * draw tool. shows how to draw with dynamic elements.
+ *
+ * MOUSE
+ * drag                : draw with text
+ *
+ * KEYS
+ * del, backspace      : clear screen
+ * arrow up            : font size +
+ * arrow down          : font size -
+ * 1 Font color red
+ * 2 Font color orange
+ * 3 Font color yellow
+ * 4 Font color green
+ * 5 Font color sky blue
+ * 6 Font color dark blue
+ * 7 Font color purple
+ * 8 Font color gray
+ * 9 Font color pink
+ * 0 Font color black
+ */
+'use strict';
+//variables are from reference material except where specified
+var x = 0;
+var y = 0;
+var stepSize = 5.0;
 
-// Globals
-let myInstance;
-let canvasContainer;
+var font = ['Georgia','Courier', 'Arial', 'cursive']; //mine
+var letters = ['BLACK','RED', 'ORANGE', 'YELLOW', 'GREEN', 'SKY', 'BLUE', 'PURPLE', 'GRAY', 'PINK'];//mine
+var fontSizeMin = 3;
+var textColor = 0;//mine
+var fontNum = 0;//mine
+var counter = 0;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
+function setup() { //setup function from reference material
+  // use full screen size
+  createCanvas(displayWidth, displayHeight);
+  background(255);
+  cursor(CROSS);
+  x = mouseX;
+  y = mouseY;
 
-    myMethod() {
-        // code to run when method is called
-    }
+  
+  textAlign(LEFT);
+  fill(0);
+  
+  
 }
 
-// setup() function is called once when the program starts
-function setup() {
-    // place our canvas, making it fit our container
-    canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
-    canvas.parent("canvas-container");
-    // resize canvas is the page is resized
-    $(window).resize(function() {
-        console.log("Resizing...");
-        resizeCanvas(canvasContainer.width(), canvasContainer.height());
-    });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
-}
-
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  textFont(font[fontNum]); //mine
+  //below from ref with minor modifications
+  if (mouseIsPressed && mouseButton == LEFT) {
+    var d = dist(x, y, mouseX, mouseY);
+    textSize(fontSizeMin + d / 2);
+    var newLetter = letters[textColor].charAt(counter);
+    stepSize = textWidth(newLetter);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+    if (d > stepSize) {
+      var angle = atan2(mouseY - y, mouseX - x);
+
+      push();
+      translate(x, y);
+      rotate(angle);
+      //above from ref with minor modifications
+      //below is mine
+      switch(textColor){
+        case 0: fill(0);
+          break;
+        case 1: fill('red');
+          break;
+        case 2: fill('orange');
+          break;
+        case 3: fill('yellow');
+          break;
+        case 4: fill('green');
+          break;
+        case 5: fill('lightblue');
+          break; 
+        case 6: fill('blue');
+          break;
+        case 7: fill('purple');
+          break;
+        case 8: fill('lightgray');
+          break;
+        case 9: fill('pink');
+          break;
+      }//above is mine
+      text(newLetter, 0, 0); //from ref
+      pop(); //from ref
+
+      counter++;//from ref
+      if (counter >= letters[textColor].length){//from ref with modifications
+         counter = 0;
+        fontNum++;//mine
+          if (fontNum>=font.length){//mine
+            fontNum=0;//mine
+          }
+      }
+      //below from ref
+      x = x + cos(angle) * stepSize;
+      y = y + sin(angle) * stepSize;
+    }
+  }
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
 function mousePressed() {
-    // code to run when mouse is pressed
+  x = mouseX;
+  y = mouseY;
+}
+
+function keyReleased() {
+  if (keyCode == DELETE || keyCode == BACKSPACE) background(255);
+}
+
+function keyPressed() {//below is mine
+  if (keyCode == UP_ARROW) fontSizeMin += 2;
+  if (keyCode == DOWN_ARROW && fontSizeMin >2) fontSizeMin -= 2;
+  if (key == '0') textColor = 0;
+  if (key == '1') textColor = 1;
+  if (key == '2') textColor = 2;
+  if (key == '3') textColor = 3;
+  if (key == '4') textColor = 4;
+  if (key == '5') textColor = 5;
+  if (key == '6') textColor = 6;
+  if (key == '7') textColor = 7;
+  if (key == '8') textColor = 8;
+  if (key == '9') textColor = 9;
+  
 }
